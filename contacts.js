cb80e22ae2e486a77c3faa74b51a1f42d5df17b9
OlVin69 <aviniichuk@gmail.com>
2024-05-08 13:10:09 +0300

    update contacts

--- contacts.js
+++ contacts.js
@@ -7,7 +7,7 @@ const contactsPath = path.join(__dirname, 'db/contacts.json');
 async function listContacts() {
     try {
       const data = await fs.readFile(contactsPath, 'utf-8');
-         contacts = JSON.parse(data);
+      const  contacts = JSON.parse(data);
         return contacts;
     } catch (error) {
       return [];
